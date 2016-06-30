b54a4597fe59d9c6f4e8314bfc24be171491f706
phuoclv <phuoc1389@gmail.com>
2016-06-30 15:04:41 +0700

    update

--- plugin.video.cantobumedia/autorun.py
+++
@@ -1,2 +0,0 @@
-import os, xbmc
-xbmc.executebuiltin("RunAddon(plugin.video.cantobumedia)")
--- plugin.video.cantobumedia/resources/lib/function.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-import xbmc,xbmcaddon,xbmcplugin,xbmcgui,sys,urllib,urllib2,re,os,codecs,unicodedata,base64
-import simplejson as json
-
-
